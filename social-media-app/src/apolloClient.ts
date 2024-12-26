ca138579b0ccacc64d1f63521ebef3851889c8d4
Devang <rakholiyadevang@gmail.com>
2024-12-26 11:11:43 +0530

    vercel.json done

--- social-media-app/src/apolloClient.ts
+++ social-media-app/src/apolloClient.ts
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'http://localhost:4000/graphql', // Replace with your GraphQL server URL
+    uri: `${import.meta.env.VITE_BACKEND_URL}`, // Replace with your GraphQL server URL
   }),
   cache: new InMemoryCache(),
 });
